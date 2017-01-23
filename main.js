5f7f7e75aef83bd793c3253a3a7ba91c7af14404
njohnstud <n.e.johnston@hotmail.com>
2017-01-22 21:24:43 -0800

    Adding carousel, finished fixed scroll bar

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-$(document).ready(function(e){
-    e.preventDefault();
-    $('.submit').on('click', '.button', function(){
-
-    });
+$(function (){
+$('.carousel').flickity({
+    cellAlign: 'center',
+    contain: true,
+    percentPostition: true,
+    imagesLoaded: true,
+    autoPlay: true,
+    prevNexButtons: false
 });
+});
+// $(document).ready(function(e){
+//     e.preventDefault();
+//     $('.submit').on('click', '.button', function(){
+//         if (input type email = value email then return alert string "Thank You for Subscribing!"
+//         else return alert string "Please enter valid email."
+//     });
+// });
